d627c0cb92efa0dae888311ae5311a4223e45925
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 07:38:13 -0300

    Explorando a classe file

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
-
         File arquivo = new File("teste.txt");
-        if(!arquivo.exists()){
-            try {
-                arquivo.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+
+        System.out.println("Tamanho: "+arquivo.length());
+        System.out.println("Diretório: "+arquivo.getAbsolutePath());
+        System.out.println("Pai: " +arquivo.getAbsoluteFile().getParent());
+        System.out.println("Leitura: "+arquivo.canRead());
+        System.out.println("Escrita: "+arquivo.canWrite());
+        System.out.println("Execução: "+arquivo.canExecute());
+        System.out.println("Modificação: "+ arquivo.lastModified());
 
     }
-
 }
