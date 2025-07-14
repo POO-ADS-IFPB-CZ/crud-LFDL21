3bf24c861b2264b8aabbc41ea9b6f12a787fdd0c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 09:06:50 -0300

    Testando o salvamento e a listagem

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import dao.AlunoDao;
+import model.Aluno;
+
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            AlunoDao dao = new AlunoDao();
+//            dao.salvar(new Aluno(456,"Maria","ADS"));
+            System.out.println(dao.getAlunos());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
