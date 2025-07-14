cb6d7e091d073e7eb0cd031db506548ee1b59600
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 08:08:54 -0300

    Lendo o arquivo

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         File arquivo = new File("teste.txt");
-        Aluno aluno = new Aluno(456, "Maria", "ADS");
 
-        try(BufferedWriter bw = new BufferedWriter(
-                new FileWriter(arquivo, true)
-        )) {
-            bw.write(aluno.getMatricula()+","+aluno.getNome()+","
-                    +aluno.getCurso());
-            bw.newLine();
+        //Lendo o arquivo
+        try(BufferedReader br = new BufferedReader(
+                new FileReader(arquivo)
+        )){
+            br.lines().forEach(System.out::println);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
+        //Escrevendo no arquivo
+//        Aluno aluno = new Aluno(456, "Maria", "ADS");
+//        try(BufferedWriter bw = new BufferedWriter(
+//                new FileWriter(arquivo, true)
+//        )) {
+//            bw.write(aluno.getMatricula()+","+aluno.getNome()+","
+//                    +aluno.getCurso());
+//            bw.newLine();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
