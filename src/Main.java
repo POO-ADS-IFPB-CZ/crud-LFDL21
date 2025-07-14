08fc72220fffbba1e81bb6b67b34dcf37329797c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 07:52:45 -0300

    Escrevendo em arquivo de texto

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         File arquivo = new File("teste.txt");
 
-        System.out.println("Tamanho: "+arquivo.length());
-        System.out.println("Diretório: "+arquivo.getAbsolutePath());
-        System.out.println("Pai: " +arquivo.getAbsoluteFile().getParent());
-        System.out.println("Leitura: "+arquivo.canRead());
-        System.out.println("Escrita: "+arquivo.canWrite());
-        System.out.println("Execução: "+arquivo.canExecute());
-        System.out.println("Modificação: "+ arquivo.lastModified());
-
+        try(BufferedWriter bw = new BufferedWriter(
+                new FileWriter(arquivo, false)
+        )) {
+            bw.write("Hello World");
+            bw.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
