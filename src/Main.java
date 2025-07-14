1824ef81cb17a0f3d2cbcf2ecf8a8dbcd6a1f557
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 08:15:47 -0300

    Usando arquivos binários

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         File arquivo = new File("teste.txt");
+        Aluno aluno = new Aluno(123, "João", "ADS");
 
-        //Lendo o arquivo
-        try(BufferedReader br = new BufferedReader(
-                new FileReader(arquivo)
+        try(ObjectOutputStream out = new ObjectOutputStream(
+                new FileOutputStream(arquivo)
         )){
-            br.lines().forEach(System.out::println);
+            out.writeObject(aluno);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -16,16 +16,5 @@ public class Main {
         }
 
 
-        //Escrevendo no arquivo
-//        Aluno aluno = new Aluno(456, "Maria", "ADS");
-//        try(BufferedWriter bw = new BufferedWriter(
-//                new FileWriter(arquivo, true)
-//        )) {
-//            bw.write(aluno.getMatricula()+","+aluno.getNome()+","
-//                    +aluno.getCurso());
-//            bw.newLine();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 }
