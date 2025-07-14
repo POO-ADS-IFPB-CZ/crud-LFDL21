fc3f772ea20d2c20d8eb756a2b0adf1a2ee60db8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-07-14 08:00:43 -0300

    Escrevendo objeto no arquivo

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         File arquivo = new File("teste.txt");
+        Aluno aluno = new Aluno(456, "Maria", "ADS");
 
         try(BufferedWriter bw = new BufferedWriter(
-                new FileWriter(arquivo, false)
+                new FileWriter(arquivo, true)
         )) {
-            bw.write("Hello World");
+            bw.write(aluno.getMatricula()+","+aluno.getNome()+","
+                    +aluno.getCurso());
             bw.newLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
